a53ec5c893d82311201cd85ba369b7fb7db02905
Swaroop <swaroop.striker@gmail.com>
2012-01-22 16:04:59 +0530

    Layout Design

--- src/index.php
+++ src/index.php
@@ -1,3 +1,97 @@
-<?php
-	echo "Hello World!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>CellBay - Your Online Mobile Store</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+      }
+    </style>
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+  </head>
+
+  <body>
+
+    <div class="topbar">
+      <div class="fill">
+        <div class="container">
+          <a class="brand" href="#">CellBay</a>
+          <ul class="nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+        <h1>Hello, world!</h1>
+        <p>Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+        <p><a class="btn primary large">Learn more &raquo;</a></p>
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="span-one-third">
+          <h2>Mobile 1</h2>
+              <ul class="media-grid">
+    <li>
+    <a href="#">
+    <img class="thumbnail" src="http://placehold.it/250x180" alt="">
+    </a>
+    </li>
+    </ul>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+        <div class="span-one-third">
+          <h2>Mobile 2</h2>
+               <ul class="media-grid">
+    <li>
+    <a href="#">
+    <img class="thumbnail" src="http://placehold.it/250x180" alt="">
+    </a>
+    </li>
+    </ul>
+ <p><a class="btn" href="#">View details &raquo;</a></p>
+       </div>
+        <div class="span-one-third">
+          <h2>Mobile 3</h2>
+             <ul class="media-grid">
+    <li>
+    <a href="#">
+    <img class="thumbnail" src="http://placehold.it/250x180" alt="">
+    </a>
+    </li>
+    </ul>
+ <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
+      </div>
+
+      <footer>
+        <p>&copy CellBay 2012</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+  </body>
+</html>
