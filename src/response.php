ab49ffb3971f15faff6acaf1d325cdc0ecf99541
Swaroop <swaroop.striker@gmail.com>
2012-01-25 12:33:25 +0530

    functions for signup

--- src/response.php
+++ src/response.php
@@ -1,13 +1,17 @@
 <?php
 //echo $_GET['module'];
+include("includes/config.php");
 if(isset($_GET['class'])){
 $class=$_GET['class'];
 switch($class){
-case 'users':	include("includes/class.user.php"); 
+case 'users':	include("includes/class.users.php"); 
 		if(isset($_GET['module'])){
 		switch($_GET['module']){
-		case 'signup':  $fields=array('');
-			 	$values=array();
+		case 'signup':  $fields=array('UserName','UserEmail','UserPassword');
+			 	$values=array($_POST['uname'],$_POST['uemail'],md5($_POST['upassword']));
+			 	$obj=new users();
+			 	$returnVal=$obj->create($fields,$values);
+			 	echo $returnVal;
 				break;
 		}
 		}
