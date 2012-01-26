4fa7f14ae03e0e1213e06df0486d9ed9f1f4e249
swaroopsm <swaroop.striker@gmail.com>
2012-01-27 00:14:38 +0530

    check login case

--- src/response.php
+++ src/response.php
@@ -13,9 +13,22 @@ case 'users':	include("includes/class.users.php");
 			 	$returnVal=$obj->create($fields,$values);
 			 	echo $returnVal;
 				break;
+				
+		case 'checklogin': 	$uname=$_POST['uname'];
+									$pwd=md5($_POST['password']);
+									$sql=mysql_query("SELECT * FROM users WHERE UserEmail='$uname' AND UserPassword='$pwd'");
+									if(mysql_num_rows($sql)==1){
+										$_SESSION['loggedin']=true;
+										$_SESSION['uname']=$uname;
+										header("location: dashboard.php");
+									}
+									else
+										header("location: index.php");
+									break;
 		}
 		}
+		break;
 	}
-	break;
+	
 }
 ?>
