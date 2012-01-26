b344e52abbb244feecdafe1a8ceba5827967504a
swaroopsm <swaroop.striker@gmail.com>
2012-01-26 23:50:03 +0530

    added view function and get private varialble attributes values

--- src/includes/class.users.php
+++ src/includes/class.users.php
@@ -14,7 +14,63 @@ echo "Constructor!";
 }
 
 public function view($uid){
+	$sql = mysql_query("SELECT * FROM users WHERE UserID=$uid");
+	$row=mysql_fetch_assoc($sql);
+	$this->uid=$uid;
+	$this->uname=$row['UserName'];
+	$this->uemail=$row['UserEmail'];
+	$this->upwd=$row['UserPassword'];
+	$this->ugender=$row['UserGender'];
+	$this->udob=$row['UserDOB'];
+	$this->ujdate=$row['UserJoinDate'];
+	$this->uprof=$row['UserProfession'];
+	$this->uimage=$row['UserImage'];
+	$this->torders=$row['TotalOrders'];
+	$this->loid=$row['LastOrderID'];
+}
 
+public static getUID(){
+	return $this->uid;
+}
+
+public static getUname(){
+	return $this->uname;
+}
+
+public static getUEmail(){
+	return $this->uemail;
+}
+
+public static getUPwd(){
+	return $this->upwd;
+}
+
+public static getUGender(){
+	return $this->ugender;
+}
+
+public static getUDOB(){
+	return $this->udob;
+}
+
+public static getUJDate(){
+	return $this->ujdate;
+}
+
+public static getUProf(){
+	return $this->uprof;
+}
+
+public static getUImage(){
+	return $this->uimage;
+}
+
+public static getTorders(){
+	return $this->torders;
+}
+
+public static getLoid(){
+	return $this->loid;
 }
 
 public function create($fields,$values){
