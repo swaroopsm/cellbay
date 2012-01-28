7f10ecd6079cead14b2e92c722de56adc0e2d4b2
swaroopsm <swaroop.striker@gmail.com>
2012-01-28 21:22:33 +0530

    added query,select,framequery and number of rows of result set functions

--- src/includes/functions.php
+++ src/includes/functions.php
@@ -11,5 +11,59 @@
 		return mysql_query($sql);
 	}
 	
+	function select($table, $rows = '*', $where = null, $order = null)  
+        { 
+            $query1 = 'SELECT '.$rows.' FROM '.$table;  
+            if($where != null)  
+                $query1 .= ' WHERE '.$where;  
+            if($order != null)  
+                $query1 .= ' ORDER BY '.$order;  
+			
+           // if($this->tableExists($table))  
+          // {  
+			//echo $query1;
+            $queryStats = dbquery($query1);  
+            if($queryStats)  
+            {  
+                $numResults = mysql_num_rows($queryStats);  
+                for($i = 0; $i < $numResults; $i++)  
+                {  
+                    $r = mysql_fetch_array($queryStats);  
+                    $key = array_keys($r);  
+                    for($x = 0; $x < count($key); $x++)  
+                    {  
+                        // Sanitizes keys so only alphavalues are allowed  
+                        if(!is_int($key[$x]))  
+                        {  
+                            if(mysql_num_rows($queryStats) > 1)  
+                                $this->result[$i][$key[$x]] = $r[$key[$x]];  
+                            else if(mysql_num_rows($queryStats) < 1)  
+                                $this->result = null;  
+                            else  
+                                $result[$key[$x]] = $r[$key[$x]];  
+                        }  
+                    }  
+                }
+                return $result;  
+            }  
+            else  
+            {  
+                return false;  
+            }  
+       }
+
+	function numRows($query){
+		return mysql_num_rows($query);
+	}
+	
+	function frameQuery($table, $rows = '*', $where = null, $order = null){
+		$query1 = 'SELECT '.$rows.' FROM '.$table;  
+            if($where != null)  
+                $query1 .= ' WHERE '.$where;  
+            if($order != null)  
+                $query1 .= ' ORDER BY '.$order;  
+			return $query1;
+	}
+	
 	connect();
 ?>
