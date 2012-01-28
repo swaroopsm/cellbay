81c0e98c35ef2e63614677623d2367faed4fb4e1
swaroopsm <swaroop.striker@gmail.com>
2012-01-28 20:32:41 +0530

    initial db functions

--- src/includes/functions.php
+++ src/includes/functions.php
@@ -1,3 +1,15 @@
 <?php
+	include("config.php");
 	
+	function connect(){
+		global $host,$user,$pwd,$db;
+		mysql_connect($host,$user,$pwd);
+		mysql_select_db($db);
+	}
+	
+	function dbquery($sql){
+		return mysql_query($sql);
+	}
+	
+	connect();
 ?>
