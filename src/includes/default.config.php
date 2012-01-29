77b755161f6584a09fee33c6ffe18a7ad4d60874
swaroop <swaroop.striker@gmail.com>
2012-01-29 22:06:04 +0530

    instructions added for default configurations

--- src/includes/default.config.php
+++ src/includes/default.config.php
@@ -1,6 +1,9 @@
 <?php
-$host="";
-$user="";
-$pwd="";
-$db="";
+/*
+* Using mysql as the database and apache as the web server
+*/
+$host="";  //Your host/server name Eg.: localhost
+$user=""; //Your server username Eg.: root
+$pwd=""; //Your server password Eg.: ******
+$db=""; //Your database name
 ?>
