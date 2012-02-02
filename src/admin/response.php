d067b937f716acd8ff1b128fb4c56e320ccd5f92
swaroop <swaroop.striker@gmail.com>
2012-02-02 23:14:44 +0530

    login module for admin

--- src/admin/response.php
+++ src/admin/response.php
@@ -1,3 +1,24 @@
 <?php
-
+include("../includes/functions.php");
+if(isset($_GET['module'])){
+	$module=$_GET['module'];
+	switch($module){
+		case 'login': $aname=$_POST['aname'];
+									$pwd=md5($_POST['apassword']);
+									$where="AdminName='$aname' AND AdminPassword='$pwd'";
+									$sql=frameQuery("admin","*",$where);
+									$query=dbquery($sql);
+									if(numRows($query)==1){
+										$_SESSION['aloggedin']=true;
+										$_SESSION['aname']=$aname;
+										$sql2=select("admin","*",$where);
+										$aid=$sql2['AdminID'];
+										$_SESSION['aid']=$aid;
+										header("location: index.php");
+									}
+									else
+										header("location: login.php");
+									break;
+	}
+}
 ?>
