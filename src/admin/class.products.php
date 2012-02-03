d1a2073bf56baa1f57a23f3942cc631d280c76ff
swaroop <swaroop.striker@gmail.com>
2012-02-03 19:51:49 +0530

    added insert and view function

--- src/admin/class.products.php
+++ src/admin/class.products.php
@@ -23,6 +23,31 @@ public function getYear(){
 return $this->year;
 }
 
+public function insert($fields,$values){
+if(!is_array($fields) || !is_array($values))
+            return 'Error - Fields and values must be sent as an array';
+                        
+        $field_ct  = count($fields);
+        $value_ct = count($values);
+        if($field_ct != $value_ct)
+            return 'Error - Field count and value count do not match.';
+                        
+        $query1 = "INSERT INTO `products` (`";
+        $query1 .= implode('`, `', $fields) . "`) VALUES ('";
+        $query1 .= implode("', '", $values) . "')";
+        $exec=dbquery($query1);
+        return $exec;
 
+}
+
+public function view($id){
+$sql=mysql_query("SELECT * FROM products WHERE ProductID=$id");
+$row=mysql_fetch_assoc($sql)
+$this->name=$row['ProductName'];
+$this->brand=$row['ProductBrand'];
+$this->price=$row['ProductPrice'];
+$this->visible=$row['ProductVisibility'];
+$this->year=$row['ProductYear'];
+}
 }
 ?>
