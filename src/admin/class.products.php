673d9770179bb1114f309dc38f5a39131d535b7d
swaroop <swaroop.striker@gmail.com>
2012-02-03 18:27:18 +0530

    declared private variables and respctive get attribute value functions

---
+++ src/admin/class.products.php
@@ -0,0 +1,28 @@
+<?php
+class products{
+
+private $name,$brand,$price,$visible,$year;
+
+public function getName(){
+return $this->name;
+}
+
+public function getBrand(){
+return $this->brand;
+}
+
+public function getPrice(){
+return $this->price;
+}
+
+public function getVisible(){
+return $this->visible;
+}
+
+public function getYear(){
+return $this->year;
+}
+
+
+}
+?>
