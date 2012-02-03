4033d128968f148dc06068c227ab288c8782fe0f
swaroop <swaroop.striker@gmail.com>
2012-02-03 17:59:55 +0530

    ui for adding product with all fields

--- src/admin/ui.php
+++ src/admin/ui.php
@@ -2,7 +2,41 @@
 if(isset($_GET['ui'])){
 $ui=$_GET['ui'];
 switch($ui){
-case 'addproduct': echo ":)";
+case 'addproduct': ?>
+<h3>Add Product</h3>
+<form>
+<table>
+<tr>
+<td><label>Product Name: 
+<td><input type="text" id="pName"/>
+</tr>
+<tr>
+<td><label>Product Brand: 
+<td><input type="text" id="pBrand"/>
+</tr>
+<tr>
+<td><label>Product Price: 
+<td><input type="text" id="pPrice"/>
+</tr>
+<tr>
+<td><label>Manufactured Year: 
+<td><input type="text" id="pYear"/>
+</tr>
+<tr>
+<td><label>Product Visibility: 
+<td><input type="text" id="pVisible"/>
+</tr>
+<tr>
+<td>
+<td>
+</tr>
+</table>
+<div style="margin-top: -20px;">
+<input style="margin-left: 40px;" class="btn success" type=submit value="Submit"/>
+<input style="margin-left: 10px;" class="btn danger" type=reset value="Reset"/>
+</div>
+</form>
+<?php
 }
 }
 ?>
