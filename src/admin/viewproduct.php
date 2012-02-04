3b92317758577fce7b040251f1f4b881fc851fda
swaroop <swaroop.striker@gmail.com>
2012-02-04 20:49:52 +0530

    added some styles for image and prints name and brand

--- src/admin/viewproduct.php
+++ src/admin/viewproduct.php
@@ -1,5 +1,10 @@
 <?php
 if(isset($_GET['productID'])){
-	echo $_GET['productID'];
+	include("../includes/functions.php");
+	include("class.products.php");
+	$id=$_GET['productID'];
+	$pobj=new products();
+	$pobj->view($id);
+	echo "<h5>".$pobj->getBrand()." ".$pobj->getName()."</h5><hr><img class='thumbnail' src='$uploads/".$pobj->getImage()."' style='width: 200px;height: 200px;border: 1px solid #dedede;padding:10px; '/>";
 }
 ?>
