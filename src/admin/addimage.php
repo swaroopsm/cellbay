14eed03cfecf5e1fa58bbab72eaa5573b35ac145
swaroop <swaroop.striker@gmail.com>
2012-02-04 18:21:12 +0530

    fecthed product id

--- src/admin/addimage.php
+++ src/admin/addimage.php
@@ -1,3 +1,7 @@
+<?php
+if(isset($_GET['productID'])){
+$pid=$_GET['productID'];
+?>
 <h5>Add Image</h5>
 <table>
 <tr><td>
@@ -5,7 +9,11 @@
 </table>
 <form id="imageform" method="post" enctype="multipart/form-data" action='ajaximage.php'>
 Upload image <input type="file" name="photoimg" id="photoimg" />
+<input type="hidden" value="<?php echo $pid; ?>">
 </form>
 <center><div id='preview'>
 </div>
 </center>
+<?php
+}
+?>
