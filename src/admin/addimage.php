0ff4e8de4741c9d854df851f28b9413ba1b9b998
swaroop <swaroop.striker@gmail.com>
2012-02-04 18:13:01 +0530

    ui for uploading image

---
+++ src/admin/addimage.php
@@ -0,0 +1,11 @@
+<h5>Add Image</h5>
+<table>
+<tr><td>
+</tr>
+</table>
+<form id="imageform" method="post" enctype="multipart/form-data" action='ajaximage.php'>
+Upload image <input type="file" name="photoimg" id="photoimg" />
+</form>
+<center><div id='preview'>
+</div>
+</center>
