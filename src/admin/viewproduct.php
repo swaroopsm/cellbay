feefbe40d51cfd3d228cb0d01705154bbd442c83
swaroop <swaroop.striker@gmail.com>
2012-02-04 21:26:00 +0530

    image float left

--- src/admin/viewproduct.php
+++ src/admin/viewproduct.php
@@ -5,6 +5,9 @@ if(isset($_GET['productID'])){
 	$id=$_GET['productID'];
 	$pobj=new products();
 	$pobj->view($id);
-	echo "<h5>".$pobj->getBrand()." ".$pobj->getName()."</h5><hr><img class='thumbnail' src='$uploads/".$pobj->getImage()."' style='width: 200px;height: 200px;border: 1px solid #dedede;padding:10px; '/>";
+	echo "<h5>".$pobj->getBrand()." ".$pobj->getName()."</h5><hr><div style='float: left;width: 300px;'><img class='thumbnail' src='$uploads/".$pobj->getImage()."' style='width: 200px;height: 200px;border: 1px solid #dedede;padding:10px; '/></div>
+	<div>
+	
+	</div>";
 }
 ?>
