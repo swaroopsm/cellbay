4d292f876d88a870bc97bf6bb80b0f80a3f89e15
swaroop <swaroop.striker@gmail.com>
2012-02-05 00:33:56 +0530

    fetched all fields in input box and added save button

--- src/admin/viewproduct.php
+++ src/admin/viewproduct.php
@@ -5,9 +5,57 @@ if(isset($_GET['productID'])){
 	$id=$_GET['productID'];
 	$pobj=new products();
 	$pobj->view($id);
-	echo "<h5>".$pobj->getBrand()." ".$pobj->getName()."</h5><hr><div style='float: left;width: 300px;'><img class='thumbnail' src='$uploads/".$pobj->getImage()."' style='width: 200px;height: 200px;border: 1px solid #dedede;padding:10px; '/></div>
-	<div>
+	echo "<div style='width: 550px;'><h5>".$pobj->getBrand()." ".$pobj->getName()."</h5><hr><div style='float: left;width: 250px;'><img class='thumbnail' src='$uploads/".$pobj->getImage()."' style='width: 200px;height: 200px;border: 1px solid #dedede;padding:10px; '/></div>
+	<div	style='font-size: 13px;'>
 	
-	</div>";
+	<tr>
+		<td>Brand: &nbsp;&nbsp;&nbsp; 
+		<td><input style='color: #000;' id='pBrand' value='".$pobj->getBrand()."'/>
+	</tr>
+	<tr>
+		<td><br><br>
+	</tr>
+	<tr>
+		<td>Model: &nbsp;&nbsp;&nbsp;
+		<input style='color: #000;' id='pName' value='".$pobj->getName()."'/>
+	</tr>
+		<tr>
+		<td><br><br>
+	</tr>
+	<tr>
+		<td>Price: &nbsp;&nbsp;&nbsp;&nbsp; 
+		<input style='color: #000;' id='pBrand' value='".$pobj->getPrice()."'/>
+	</tr>
+	<tr>
+		<td><br><br>
+	</tr>
+		<tr>
+		<td>Year: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+		<input style='color: #000;' id='pYear' value='".$pobj->getYear()."'/>
+	</tr>
+		<tr>
+		<td><br><br>
+	</tr>
+	<tr>
+		<td>Visibility: &nbsp; 
+		<input style='color: #000;' id='pYear' value='".$pobj->getYear()."'/>
+	</tr>
+	<tr>
+		<td><br><br>
+	</tr>
+	<tr>
+		<td><button style='float: right;margin-right: 240px;' id='save' class='btn success'>Save</button>
+	</tr>
+		<tr>
+		<td><br><br>
+	</tr>
+	</div></div>";
 }
 ?>
+<script type='text/javascript'>
+$(document).ready(function(){
+$("#save").click(function(){
+
+});
+});
+</script>
