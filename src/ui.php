1099e227762613ea9b11c64e17a3854e607bb8ec
swaroop <swaroop.striker@gmail.com>
2012-02-05 22:52:16 +0530

    results of the phones fetched, displayed

--- src/ui.php
+++ src/ui.php
@@ -7,13 +7,39 @@ case 'getPhoneBrand': $brand=$_POST['brand'];
 											$where="ProductBrand='".$brand."'";
 											$query=frameQuery("products","*",$where);
 											$num=numRows(mysql_query($query));
+											echo "<div class='row'>";
 											if($num>1){
 												$row=select("products","*",$where);
-												
+												echo "<div class='span-one-third'>
+												<h2>".$row[$i]['ProductName']."</h2>
+             						 <ul class='media-grid'>
+										    <li>
+									    <a href='#'>
+										   <img class='thumbnail' src='".substr($uploads,3)."/".$row[$i]['ProductImage']."' width='250' height='250' alt='Samsung'>
+								    </a>
+									    </li>
+							    </ul>
+    
+				        </div>";
+											}
+											else if($num==1){
+												$row=select("products","*",$where);
+												echo "<div class='span-one-third'>
+												<h2>".$row['ProductName']."</h2>
+             						 <ul class='media-grid'>
+										    <li>
+									    <a href='#'>
+										   <img class='thumbnail' src='".substr($uploads,3)."/".$row['ProductImage']."' width='250' height='120' alt='Samsung'>
+								    </a>
+									    </li>
+							    </ul>
+    
+				        </div>";
 											}
 											else{
-												$row=select("products","*",$where);
+											echo "<center><h1><br>There are no phones that are availbale for this brand</h1></center>";
 											}
+											echo "</div>";
 											break;
  ?>
 
