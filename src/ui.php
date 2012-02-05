2d48fcce3161c2a9c4888e613869d87da3ca0807
swaroop <swaroop.striker@gmail.com>
2012-02-05 22:31:31 +0530

    query for returning rows and data of selected brand

--- src/ui.php
+++ src/ui.php
@@ -1,8 +1,21 @@
 <?php
 if(isset($_GET['module'])){
+include("includes/functions.php");
 $module=$_GET['module'];
 switch($module){
-case 'getPhoneBrand': ?>
+case 'getPhoneBrand': $brand=$_POST['brand'];
+											$where="ProductBrand='".$brand."'";
+											$query=frameQuery("products","*",$where);
+											$num=numRows(mysql_query($query));
+											if($num>1){
+												$row=select("products","*",$where);
+												
+											}
+											else{
+												$row=select("products","*",$where);
+											}
+											break;
+ ?>
 
 
 
