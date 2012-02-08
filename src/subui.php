1342281575b53461c52a0b8f18356d9503a7c189
swaroop <swaroop.striker@gmail.com>
2012-02-08 18:33:54 +0530

    echo details of phone

--- src/subui.php
+++ src/subui.php
@@ -7,9 +7,21 @@ switch($module){
 case 'myphone': $pid=$_GET['pid'];
 								$pobj=new products();
 								$pobj->view($pid);
-								echo "<div class='modal-header'>
+								echo "<div style='width: 500px;'><div class='modal-header'>
 								<h3>".$pobj->getBrand()." ".$pobj->getName()."</h3>
-								</div><div class='modal-body'><img src='$uploads/".$pobj->getImage()."' width='400' height='400'/>".$pobj->getBrand()."</div>";
+								</div><div class='modal-body'><div style='float: left;'><img src='$uploads/".$pobj->getImage()."' width='250' height='200'/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+								<div>
+								<tr>
+									<td><b>Model Name: </b><td>".$pobj->getName()."	
+								</tr>
+								<tr>
+									<td><br>
+								</tr>
+								<tr>
+									<b>Price: </b><td>Rs. ".$pobj->getPrice()."	
+								</tr>
+								</div>
+								</div></div>";
 break;
 }
 }
