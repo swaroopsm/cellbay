990537f5d5eded74948bf9111487b4b3c8c99ef8
swaroop <swaroop.striker@gmail.com>
2012-02-08 20:30:08 +0530

    added a comment on how to create config file

--- src/includes/default.config.php
+++ src/includes/default.config.php
@@ -1,5 +1,6 @@
 <?php
 /*
+* Create a file called config.php in the includes/ directory, copy and paste the below code with the appropriate   * values for each variable
 * Using mysql as the database and apache as the web server
 */
 $host="";  //Your host/server name Eg.: localhost
