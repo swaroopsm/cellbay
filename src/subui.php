3225a69814a6177bc967e8872242917ba0330ec4
swaroop <swaroop.striker@gmail.com>
2012-02-08 19:58:01 +0530

    adjusted margin for txt sign in to buy

--- src/subui.php
+++ src/subui.php
@@ -7,11 +7,11 @@ switch($module){
 case 'myphone': $pid=$_GET['pid'];
 								$pobj=new products();
 								$pobj->view($pid);
-								echo "<div style='width: 600px;'><div class='modal-header'>
+								echo "<div style='width: 550px;'><div class='modal-header'>
 								<h3>".$pobj->getBrand()." ".$pobj->getName()."</h3>
-								</div><div class='modal-body'><div style='float: left; style='width: 300px; height: 250px;border:1px solid red;'><img src='$uploads/".$pobj->getImage()."' width='250' height='200'/></div>
+								</div><div class='modal-body'><div style='float: left;width: 500px; height: 350px;border:1px solid #eeeeee;padding: 	5px;'><img width='500' height='350' src='$uploads/".$pobj->getImage()."'/></div>
 								<div>
-								<table style='width: 300px;'>
+								<center><table style='width: 400px;'>
 								<tr>
 									<td><b>Model Name: </b>
 									<td>".$pobj->getName()."	
@@ -22,13 +22,14 @@ case 'myphone': $pid=$_GET['pid'];
 								</tr>
 								
 								<tr>
-									<td><b>Manf. Year: </b><td>Rs. ".$pobj->getYear()."	
+									<td><b>Manufactured Year: </b><td>Rs. ".$pobj->getYear()."	
 								</tr>
 								<tr>
 									<td>
 									<td>
 								</tr>
 								</table>
+								<h4 style='margin-top: -20px;'>Sign In to buy this product</h4></center>
 								</div>
 								</div></div>";
 break;
