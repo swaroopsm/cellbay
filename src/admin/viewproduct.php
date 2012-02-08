7564b9321eca8f5f11d10f6f7e47d87477b13c81
swaroop <swaroop.striker@gmail.com>
2012-02-08 18:18:35 +0530

    added ../ prefixed to

--- src/admin/viewproduct.php
+++ src/admin/viewproduct.php
@@ -5,7 +5,7 @@ if(isset($_GET['productID'])){
 	$id=$_GET['productID'];
 	$pobj=new products();
 	$pobj->view($id);
-	echo "<div style='width: 550px;'><h5>".$pobj->getBrand()." ".$pobj->getName()."</h5><hr><div style='float: left;width: 250px;'><img class='thumbnail' src='$uploads/".$pobj->getImage()."' style='width: 200px;height: 200px;border: 1px solid #dedede;padding:10px; '/></div>
+	echo "<div style='width: 550px;'><h5>".$pobj->getBrand()." ".$pobj->getName()."</h5><hr><div style='float: left;width: 250px;'><img class='thumbnail' src='../$uploads/".$pobj->getImage()."' style='width: 200px;height: 200px;border: 1px solid #dedede;padding:10px; '/></div>
 	<div	style='font-size: 13px;'>
 	
 	<tr>
