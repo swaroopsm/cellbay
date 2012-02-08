210f2ed33cb0c5683d4bdbb00868d750cc14e35c
swaroop <swaroop.striker@gmail.com>
2012-02-08 18:11:16 +0530

    echo image of product

--- src/subui.php
+++ src/subui.php
@@ -1,8 +1,15 @@
 <?php
 if(isset($_GET['module'])){
+include("includes/functions.php");
+include("admin/class.products.php");
 $module=$_GET['module'];
 switch($module){
-case 'myphone': 
+case 'myphone': $pid=$_GET['pid'];
+								$pobj=new products();
+								$pobj->view($pid);
+								echo "<div class='modal-header'>
+								<h3>".$pobj->getBrand()." ".$pobj->getName()."</h3>
+								</div><div class='modal-body'><img src='$uploads/".$pobj->getImage()."' width='400' height='400'/>".$pobj->getBrand()."</div>";
 break;
 }
 }
