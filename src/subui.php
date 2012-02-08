de22820e4590aef773cf2206ccca35c8290ead94
swaroop <swaroop.striker@gmail.com>
2012-02-08 18:45:52 +0530

    added table for phone details

--- src/subui.php
+++ src/subui.php
@@ -7,19 +7,28 @@ switch($module){
 case 'myphone': $pid=$_GET['pid'];
 								$pobj=new products();
 								$pobj->view($pid);
-								echo "<div style='width: 500px;'><div class='modal-header'>
+								echo "<div style='width: 600px;'><div class='modal-header'>
 								<h3>".$pobj->getBrand()." ".$pobj->getName()."</h3>
-								</div><div class='modal-body'><div style='float: left;'><img src='$uploads/".$pobj->getImage()."' width='250' height='200'/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+								</div><div class='modal-body'><div style='float: left; style='width: 300px; height: 250px;border:1px solid red;'><img src='$uploads/".$pobj->getImage()."' width='250' height='200'/></div>
 								<div>
+								<table style='width: 300px;'>
 								<tr>
-									<td><b>Model Name: </b><td>".$pobj->getName()."	
+									<td><b>Model Name: </b>
+									<td>".$pobj->getName()."	
+								</tr>
+									
+								<tr>
+									<td><b>Price: </b><td>Rs. ".$pobj->getPrice()."	
+								</tr>
+								
+								<tr>
+									<td><b>Manf. Year: </b><td>Rs. ".$pobj->getYear()."	
 								</tr>
 								<tr>
-									<td><br>
+									<td>
+									<td>
 								</tr>
-								<tr>
-									<b>Price: </b><td>Rs. ".$pobj->getPrice()."	
-								</tr>
+								</table>
 								</div>
 								</div></div>";
 break;
